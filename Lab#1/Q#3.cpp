99e92b8ac03a2e2e514b678ab6484985a2749b26
Ahmed Raza <muhammadahmedrazartx@gmail.com>
2026-09-16 19:36:25 +0500

    reamains-Question

---
+++ Lab#1/Q#3.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class MedianFinder
+{
+private:
+    vector<int> nums;
+public:
+
+    MedianFinder()
+    {
+    }
+
+    void addNum(int num)
+    {
+        nums.push_back(num);
+    }
+
+    double findMedian()
+    {
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+
+        if (n % 2 == 1)
+            return nums[n / 2];
+        }
+        else
+        {
+            return (nums[n / 2 - 1] + nums[n / 2]) / 2.0;
+        }
+    }
+};
+
+int main()
+{
+    MedianFinder medianFinder;
+
+    medianFinder.addNum(1);
+    medianFinder.addNum(2);
+
+    cout << "Median = " << medianFinder.findMedian() << endl;
+    medianFinder.addNum(3);
+    cout << "Median = " << medianFinder.findMedian() << endl;
+
+    return 0;
+}
