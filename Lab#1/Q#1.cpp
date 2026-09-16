d9916a3beeed452ba8a235cc9c77453ec1c1236f
Ahmed Raza <muhammadahmedrazartx@gmail.com>
2026-09-16 19:15:09 +0500

    first commit

---
+++ Lab#1/Q#1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows, cols;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    cout << "Enter number of columns: ";
+    cin >> cols;
+
+    int arr[100][100];
+    int oneD[10000];
+    cout << "Enter elements\n";
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    int k = 0;
+
+    for (int j = 0; j < cols; j++)
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            oneD[k] = arr[i][j];
+            k++;
+        }
+    }
+
+    cout << "\n1D Array in Column Major Order:\n";
+
+    for (int i = 0; i < k; i++)
+    {
+        cout << oneD[i] << " ";
+    }
+
+    return 0;
+}
